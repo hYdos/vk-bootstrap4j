969d0f25e0eabb3b329134db9d37aef9e46e1492
hYdos <haydenv06@gmail.com>
2023-12-26 09:59:36 +1000

    proper publishing config

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,8 +9,23 @@ repositories {
 }
 
 dependencies {
-    implementation(platform("org.lwjgl:lwjgl-bom:3.3.4-SNAPSHOT"))
+    implementation(platform("org.lwjgl:lwjgl-bom:3.3.4"))
 
     implementation("org.lwjgl", "lwjgl")
     implementation("org.lwjgl", "lwjgl-vulkan")
 }
+
+
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "me.hydos"
+            artifactId = "vk-bootstrap4j"
+            version = "1.0"
+
+            from(components["java"])
+        }
+    }
+}
+
